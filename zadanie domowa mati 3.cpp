9d43cad596aa66308d7ee1daabd104682ebc0c3f
Mati1515 <mateo.krupa@gmail.com>
2016-07-15 15:11:59 +0200

    Nowa zmiana

---
+++ zadanie domowa mati 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+	int n;
+	
+	cout << "wpisz liczbe ";
+	cin >> n;
+	cout << endl;
+	cout << endl;
+	while(n>0){
+		n=n-1;
+		cout << "petla";
+		cout << endl;
+		cout << endl;
+	}
+		
+	return 0;
+}
+
---
+++ zadanie domowe 3-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+	int n;
+	
+	cout << "wpisz liczbe od 1 do ..."<< endl;
+	cin >> n;
+	cout << endl;
+	cout << endl;
+for(int n=0;;){
+	n=n-1;
+	cout<<"petla"<<endl<<endl;
+}
+	
+	
+	return 0;
+}
