121160e975d106c395700d5351afeeaabdee33c1
Mati1515 <mateo.krupa@gmail.com>
2016-07-15 16:03:55 +0200

    program lekcja

--- lekcja1.cpp
+++ lekcja1.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+
+using namespace std;
+
+int main() {
+	int a=10,b=2;
+	
+	while(b>0){
+		a = 10;
+		while( (a<=11) || (a>=20) ) {
+			cout << "podaj liczbe mniejsza od 20 i wieksza od 11: ";
+			cin >> a;
+			if((a>11)&&(a<20)){
+				cout << "prawda " << endl << endl;
+			}
+			else{
+				cout << "falsz" << endl << endl;
+			}
+		}
+		
+		b=b-1;
+	}
+	
+	
+	return 0;
+}
